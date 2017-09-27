68294440fa84f15226423e9603140ab8b044b538
David <admin@dxscx.com>
2017-09-27 12:58:05 +0800

    feat: optimize num002

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 | **Number** | **Title** | **Solution** | **Type** | **Difficulty** |**Complexity(T)** |**Complexity(S)** |**Time** |**Beats** |
 | :---: |  :---:  |  :---: |  :---: |  :---:  | :---: |  :---:  |  :---:  |  :---:  |
 | 001 | [Two Sum](https://leetcode.com/problems/two-sum/) | [num001.py](/python/num001.py) | Hash Table | Easy |O(n)|O(n)| 32 ms |98.23%|
-| 002 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/) | [num002.py](/python/num002.py) | Linked List | Medium |O(max(m,n)) | O(max(m,n))| 125 ms	 |57.29%|
+| 002 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/) | [num002.py](/python/num002.py) | Linked List | Medium |O(m+n) | O(m+n)| 125 ms	 |57.29%|
+| 002_2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/) | [num002.py](/python/num002_2.py) | Linked List | Medium |O(max(m,n) | O(max(m,n)| 106 ms	 |99.06%|
---
+++ python/num002_2.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: David 
+@contact: tangwei@newrank.cn
+@file: num002_2.py 
+@time: 2017/9/27 12:25 
+@description: 
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        >>> Solution.get_num_str_from_node(Solution().addTwoNumbers(Solution.construct_nodes("5"), Solution.construct_nodes("5")))
+        10
+        """
+        head = ListNode(0)
+        current_node = head
+        ten_digit = 0
+        while l1 or l2 or ten_digit:
+            num1, num2 = 0, 0
+            if l1:
+                num1 = l1.val
+                l1 = l1.next
+            if l2:
+                num2 = l2.val
+                l2 = l2.next
+            temp_sum = num1 + num2 + ten_digit
+            digit = temp_sum % 10
+            current_node.next = ListNode(digit)
+            ten_digit = temp_sum / 10
+            current_node = current_node.next
+        return head.next
+
+    @classmethod
+    def get_num_str_from_node(cls, node):
+        """
+        implement by list (str is alternative)
+        :param node: 
+        :return: 
+        """
+        result = list()  # support long node
+        while node is not None:
+            result.append(str(node.val))
+            node = node.next
+        return int("".join(result[::-1]))
+
+    @classmethod
+    def construct_nodes(cls, num_str):
+        """
+        :param num_str: not allow None or ""
+        :return: 
+        """
+        head = ListNode(int(num_str[0]))
+        current_node = head
+        for x in num_str[1:]:
+            current_node.next = ListNode(int(x))
+            current_node = current_node.next
+        return head
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod(verbose=True)
